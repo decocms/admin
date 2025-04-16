c9f857c4d4d4c94f774791d3717c950a451c621e
gimenes <tlgimenes@gmail.com>
2025-04-16 16:17:23 -0300

    fix agent cards

--- apps/web/src/components/agents/list.tsx
+++ apps/web/src/components/agents/list.tsx
@@ -414,7 +414,7 @@ export default function List() {
         : agents.length > 0
         ? (
           <>
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-6 peer">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-5 gap-6 peer">
               {filteredAgents?.map((agent) => (
                 <AgentCard key={agent.id} agent={agent} />
               ))}

6cf30d2378224cf9356b69a8a1648d3fd0d91082
Héricles <hericles.me@icloud.com>
2025-04-23 18:06:58 -0300

    fix: preview sizing

--- apps/web/src/components/dock/index.tsx
+++ apps/web/src/components/dock/index.tsx
@@ -53,7 +53,7 @@ const adapter =
           </div>
         }
       >
-        {props.api.component === mainViewName
+        {props.api.component === mainViewName || props.api.component === "preview"
           ? <Component {...props.params} />
           : (
             <ScrollArea className="h-full w-full px-4 py-2 bg-gradient-to-b from-white to-slate-50 p-6 text-slate-700">

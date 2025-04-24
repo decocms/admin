48ba55d443fc915556413fbc823cf8c937d64d67
igorbrasileiro <brasileiro456@gmail.com>
2025-04-24 09:12:24 -0300

    fix preview height

--- apps/web/src/components/dock/index.tsx
+++ apps/web/src/components/dock/index.tsx
@@ -56,7 +56,7 @@ const adapter =
         {props.api.component === mainViewName
           ? <Component {...props.params} />
           : (
-            <ScrollArea className="h-full w-full px-4 py-2 bg-gradient-to-b from-white to-slate-50 p-6 text-slate-700 ">
+            <ScrollArea className="h-full w-full px-4 py-2 bg-gradient-to-b from-white to-slate-50 p-6 text-slate-700 [&>[data-slot='scroll-area-viewport']>div]:h-full">
               <Component {...props.params} />
             </ScrollArea>
           )}

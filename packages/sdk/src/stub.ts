7b3d692ff5074e60d5c8d0aed1af83d0abedff85
Héricles <hericles.me@icloud.com>
2025-04-25 08:20:21 -0300

    fix: types

--- packages/sdk/src/stub.ts
+++ packages/sdk/src/stub.ts
@@ -34,7 +34,7 @@ export const stub = <T extends Actor>(name: string) => {
           return fetch(url, {
             ...options,
             headers: {
-              ...(options?.headers || {}),
+              ...(options?.headers as HeadersInit || {}),
               "x-trace-debug-id": getTraceDebugId(),
             },
           });

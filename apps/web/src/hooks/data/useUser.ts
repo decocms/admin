a2031c98f6c5234301f5e1aafe8430754b73b4f2
igorbrasileiro <brasileiro456@gmail.com>
2025-04-29 19:20:28 -0300

    use fetchUser from sdk at useUser

--- apps/web/src/hooks/data/useUser.ts
+++ apps/web/src/hooks/data/useUser.ts
@@ -1,4 +1,4 @@
-import { AUTH_URL, callTool } from "@deco/sdk";
+import { fetchUser } from "@deco/sdk";
 import { use } from "react";
 
 export interface User {
@@ -26,25 +26,6 @@ export class NotLoggedInError extends Error {
     this.name = "NotLoggedInError";
   }
 }
-
-const fetchUser = async (): Promise<User> => {
-  try {
-    const { content } = await callTool({
-      type: "HTTP",
-      url: new URL("/mcp", AUTH_URL).href,
-    }, {
-      arguments: {},
-      name: "PROFILES_GET",
-    });
-    const [{ text }] = content as [{ text: string }];
-    const { content: [{ text: stringData }] } = JSON.parse(text);
-    const user = JSON.parse(stringData);
-
-    return user;
-  } catch (_) {
-    throw new NotLoggedInError("User is not logged in");
-  }
-};
 
 const promise = fetchUser();
 

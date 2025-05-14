e992fa8c4d3ebca6b3a4dcdceb63e67954a9b18c
Igor Brasileiro <brasileiro456@gmail.com>
2025-05-13 22:18:34 -0300

    first try react compiler

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -2,8 +2,19 @@ import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
 
+// https://react.dev/learn/react-compiler#usage-with-vite
+const ReactCompilerConfig = {
+  target: '19' // '17' | '18' | '19'
+};
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react({
+        babel: {
+          plugins: [
+            ["babel-plugin-react-compiler", ReactCompilerConfig],
+          ],
+        },
+      }), tailwindcss()],
   server: { port: 3000 },
 });

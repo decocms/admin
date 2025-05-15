e09475817ea8ac5b5eff9ce6b8cae892ee528786
jovenan <jovenan2001@gmail.com>
2025-05-15 17:29:24 -0300

    any

--- packages/sdk/src/models/trigger.ts
+++ packages/sdk/src/models/trigger.ts
@@ -1,6 +1,11 @@
 import { z } from "zod";
 import { AgentSchema } from "./agent.ts";
 
+// deno-lint-ignore-file no-explicit-any
+// TODO(@mcandeia) move from actors types
+export type TriggerData = any;
+export type TriggerRun = any;
+
 /**
  * Schema for tool call validation
  */

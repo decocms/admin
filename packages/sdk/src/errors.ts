9db654912bdfc7eb57a7d2fe35ff34d1b1badab6
gimenes <tlgimenes@gmail.com>
2025-05-19 19:30:21 -0300

    add x-debug-trace-id to the interface

--- packages/sdk/src/errors.ts
+++ packages/sdk/src/errors.ts
@@ -1,61 +1,71 @@
 export class HttpError extends Error {
   readonly code?: number;
-  constructor(message: string) {
+  readonly errorId?: string;
+
+  constructor(message: string, errorId?: string) {
     super(message);
+    this.errorId = errorId;
   }
 }
 
 export class UserInputError extends HttpError {
   override code = 400;
-  constructor(message: string = "User input error") {
-    super(message);
+  constructor(message: string = "User input error", errorId?: string) {
+    super(message, errorId);
   }
 }
 
 export class UnauthorizedError extends HttpError {
   override code = 401;
-  constructor(message: string = "User is not logged in") {
-    super(message);
+  constructor(message: string = "User is not logged in", errorId?: string) {
+    super(message, errorId);
   }
 }
 
 export class ForbiddenError extends HttpError {
   override code = 403;
-  constructor(message: string = "User does not have access to this resource") {
-    super(message);
+  constructor(
+    message: string = "User does not have access to this resource",
+    errorId?: string,
+  ) {
+    super(message, errorId);
   }
 }
 
 export class NotFoundError extends HttpError {
   override code = 404;
-  constructor(message: string = "Resource not found") {
-    super(message);
+  constructor(message: string = "Resource not found", errorId?: string) {
+    super(message, errorId);
   }
 }
 
 export class InternalServerError extends HttpError {
   override code = 500;
-  constructor(message: string = "Internal server error") {
-    super(message);
+  constructor(message: string = "Internal server error", errorId?: string) {
+    super(message, errorId);
   }
 }
 
-export const getErrorByStatusCode = (statusCode: number, message?: string) => {
+export const getErrorByStatusCode = (
+  statusCode: number,
+  message?: string,
+  errorId?: string,
+) => {
   if (statusCode === 400) {
-    return new UserInputError(message);
+    return new UserInputError(message, errorId);
   }
 
   if (statusCode === 401) {
-    return new UnauthorizedError(message);
+    return new UnauthorizedError(message, errorId);
   }
 
   if (statusCode === 403) {
-    return new ForbiddenError(message);
+    return new ForbiddenError(message, errorId);
   }
 
   if (statusCode === 404) {
-    return new NotFoundError(message);
+    return new NotFoundError(message, errorId);
   }
 
-  return new InternalServerError(message);
+  return new InternalServerError(message, errorId);
 };

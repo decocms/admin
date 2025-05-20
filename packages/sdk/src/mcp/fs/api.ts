1c00ab68052d953a81122d141b38138b013c6248
igorbrasileiro <brasileiro456@gmail.com>
2025-05-20 15:30:27 -0300

    add canAccess to file system

--- packages/sdk/src/mcp/fs/api.ts
+++ packages/sdk/src/mcp/fs/api.ts
@@ -11,6 +11,7 @@ import { WELL_KNOWN_ORIGINS } from "../../hosts.ts";
 import {
   assertHasWorkspace,
   assertUserHasAccessToWorkspace,
+  canAccessWorkspaceResource,
 } from "../assertions.ts";
 import { AppContext, createApiHandler, getEnv } from "../context.ts";
 
@@ -71,6 +72,9 @@ export const listFiles = createApiHandler({
   schema: z.object({
     prefix: z.string().describe("The root directory to list files from"),
   }),
+  async canAccess(_, c) {
+    return await canAccessWorkspaceResource(this.name, c);
+  },
   handler: async ({ prefix: root }, c) => {
     const bucketName = getWorkspaceBucketName(c);
 
@@ -97,6 +101,9 @@ export const readFile = createApiHandler({
       "Seconds until URL expires (default: 60)",
     ),
   }),
+  async canAccess(_, c) {
+    return await canAccessWorkspaceResource(this.name, c);
+  },
   handler: async ({ path, expiresIn = 60 }, c) => {
     const bucketName = getWorkspaceBucketName(c);
 
@@ -120,6 +127,9 @@ export const readFileMetadata = createApiHandler({
   schema: z.object({
     path: z.string(),
   }),
+  async canAccess(_, c) {
+    return await canAccessWorkspaceResource(this.name, c);
+  },
   handler: async ({ path }, c) => {
     const bucketName = getWorkspaceBucketName(c);
 
@@ -156,6 +166,9 @@ export const writeFile = createApiHandler({
       "Metadata to be added to the file",
     ),
   }),
+  async canAccess(_, c) {
+    return await canAccessWorkspaceResource(this.name, c);
+  },
   handler: async ({ path, expiresIn = 60, contentType, metadata }, c) => {
     const bucketName = getWorkspaceBucketName(c);
 
@@ -182,6 +195,9 @@ export const deleteFile = createApiHandler({
   name: "FS_DELETE",
   description: "Delete a file",
   schema: z.object({ path: z.string() }),
+  async canAccess(_, c) {
+    return await canAccessWorkspaceResource(this.name, c);
+  },
   handler: async ({ path }, c) => {
     const bucketName = getWorkspaceBucketName(c);
 

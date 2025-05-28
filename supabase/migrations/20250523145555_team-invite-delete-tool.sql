3c9c6aab6e96e79c43b03df4b5d50440dd6044ab
igorbrasileiro <brasileiro456@gmail.com>
2025-05-28 14:43:52 -0300

    make migration sql idempotent

--- supabase/migrations/20250523145555_team-invite-delete-tool.sql
+++ supabase/migrations/20250523145555_team-invite-delete-tool.sql
@@ -1,11 +1,26 @@
-UPDATE "public"."policies" SET "statements" = ARRAY[
-    '{"effect":"allow","resource":"deco-sites/admin/actions/teams/inviteMembers.ts"}'::jsonb,
-    '{"effect":"allow","resource":"deco-sites/admin/actions/invites/resendInviteEmail.ts"}'::jsonb,
-    '{"effect":"allow","resource":"deco-sites/admin/actions/invites/delete.ts"}'::jsonb,
-    '{"effect":"allow","resource":"deco-sites/admin/actions/roles/updateUserRole.ts"}'::jsonb,
-    '{"effect":"allow","resource":"deco-sites/admin/actions/teams/removeMember.ts"}'::jsonb,
-    '{"effect":"allow","resource":"TEAM_MEMBERS_UPDATE"}'::jsonb,
-    '{"effect":"allow","resource":"TEAM_MEMBERS_REMOVE"}'::jsonb,
-    '{"effect":"allow","resource":"TEAM_MEMBERS_INVITE"}'::jsonb,
+WITH current AS (
+  SELECT id, statements
+  FROM public.policies
+  -- manage_member policy id
+  WHERE id = 2
+),
+to_add AS (
+  SELECT unnest(ARRAY[
     '{"effect":"allow","resource":"TEAM_INVITE_DELETE"}'::jsonb
-] WHERE "id" = 2;
+  ]) AS new_statement
+),
+missing AS (
+  SELECT current.id, current.statements, array_agg(to_add.new_statement) AS new_statements
+  FROM current
+  JOIN to_add
+    ON NOT EXISTS (
+      SELECT 1
+      FROM unnest(current.statements) AS stmt
+      WHERE stmt = to_add.new_statement
+    )
+  GROUP BY current.id, current.statements
+)
+UPDATE public.policies
+SET statements = array_cat(missing.statements, missing.new_statements)
+FROM missing
+WHERE public.policies.id = missing.id;

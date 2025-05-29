deco-cx/chat

filter blocked roles

packages/sdk/src/mcp/members/api.ts
@@ -79,11 +79,14 @@ interface DbMember {
 
 const mapMember = (
   { member_roles, ...member }: DbMember,
+  c: AppContext,
 ) => ({
   ...member,
   // @ts-expect-error - Supabase user metadata is not typed
   profiles: userFromDatabase(member.profiles),
-  roles: member_roles.map((memberRole) => memberRole.roles).filter(isRole),
+  roles: c.policy.filterTeamRoles(
+    member_roles.map((memberRole) => memberRole.roles).filter(isRole),
+  ),
 });
 
 export const getTeamMembers = createApiHandler({
@@ -123,7 +126,7 @@ export const getTeamMembers = createApiHandler({
 
     if (error) throw error;
 
-    const members = data.map((member) => mapMember(member));
+    const members = data.map((member) => mapMember(member, c));
 
     let activityByUserId: Record<string, string> = {};
 
@@ -645,21 +648,34 @@ export const teamRolesList = createApiHandler({
     return await canAccessTeamResource(name, props.teamId, c);
   },
   handler: async (props, c) => {
-    const { teamId } = props;
-    const db = c.db;
+    return await c.policy.getTeamRoles(props.teamId);
+  },
+});
 
-    // Helper function to create the team or deco basic roles query
-    const getTeamOrDecoBasicRolesQuery = (teamId: number) => {
-      return `team_id.eq.${teamId},team_id.is.null`;
-    };
+export const updateMemberRole = createApiHandler({
+  name: "UPDATE_MEMBER_ROLE",
+  description: "Update a member's role in a team",
+  schema: z.object({
+    teamId: z.number(),
+    userId: z.string(),
+    roleId: z.number(),
+    action: z.enum(["grant", "revoke"]),
+  }),
+  async canAccess(name, props, c) {
+    return await canAccessTeamResource(name, props.teamId, c);
+  },
+  handler: async (props, c) => {
+    const { teamId, userId, roleId, action } = props;
 
-    // Get all roles for this team and deco basic roles
-    const { data, error } = await db.from("roles").select(
-      "id, name, description, team_id",
-    ).or(getTeamOrDecoBasicRolesQuery(teamId));
+    const { data: profile } = await c.db.from("profiles").select("email").eq(
+      "user_id",
+      userId,
+    ).single();
 
-    if (error) throw error;
+    if (!profile) throw new NotFoundError(`User with id ${userId} not found`);
 
-    return data;
+    await c.policy.updateUserRole(teamId, profile.email, { roleId, action });
+
+    return { success: true };
   },
 });

decocms/admin

packages/sdk/src/mcp/members/api.ts
@@ -90,13 +90,16 @@ interface DbMember {
 
 const mapMember = (
   { member_roles, ...member }: DbMember,
+  c: AppContext,
 ) => ({
   ...member,
   user_id: member.user_id ?? "",
   created_at: member.created_at ?? "",
   // @ts-expect-error - Supabase user metadata is not typed
   profiles: userFromDatabase(member.profiles),
-  roles: member_roles.map((memberRole) => memberRole.roles).filter(isRole),
+  roles: c.policy.filterTeamRoles(
+    member_roles.map((memberRole) => memberRole.roles).filter(isRole),
+  ),
 });
 
 export const getTeamMembers = createTool({
@@ -145,7 +148,7 @@ export const getTeamMembers = createTool({
 
     if (error) throw error;
 
-    const members = data.map((member) => mapMember(member));
+    const members = data.map((member) => mapMember(member, c));
     const invites = invitesData ?? [];
 
     let activityByUserId: Record<string, string> = {};
@@ -703,21 +706,34 @@ export const teamRolesList = createTool({
     return await canAccessTeamResource(name, props.teamId, c);
   },
   handler: async (props, c) => {
-    const { teamId } = props;
-    const db = c.db;
+    return await c.policy.getTeamRoles(props.teamId);
+  },
+});
 
-    // Helper function to create the team or deco basic roles query
-    const getTeamOrDecoBasicRolesQuery = (teamId: number) => {
-      return `team_id.eq.${teamId},team_id.is.null`;
-    };
+export const updateMemberRole = createTool({
+  name: "UPDATE_MEMBER_ROLE",
+  description: "Update a member's role in a team",
+  inputSchema: z.object({
+    teamId: z.number(),
+    userId: z.string(),
+    roleId: z.number(),
+    action: z.enum(["grant", "revoke"]),
+  }),
+  async canAccess(name, props, c) {
+    return await canAccessTeamResource(name, props.teamId, c);
+  },
+  handler: async (props, c) => {
+    const { teamId, userId, roleId, action } = props;
 
-    // Get all roles for this team and deco basic roles
-    const { data, error } = await db.from("roles").select(
-      "id, name, description, team_id",
-    ).or(getTeamOrDecoBasicRolesQuery(teamId));
+    const { data: profile } = await c.db.from("profiles").select("email").eq(
+      "user_id",
+      userId,
+    ).single();
 
-    if (error) throw error;
+    if (!profile) throw new NotFoundError(`User with id ${userId} not found`);
 
-    return data;
+    await c.policy.updateUserRole(teamId, profile.email, { roleId, action });
+
+    return { success: true };
   },
 });
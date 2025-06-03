659fe0f972900eebb3de29a7dc0aad040b5c54ac
viktormarinho <viktormpcs@gmail.com>
2025-06-03 14:23:34 -0300

    go

--- plugins/enforce-status-property-name.ts
+++ plugins/enforce-status-property-name.ts
@@ -1,22 +1,22 @@
 const plugin: Deno.lint.Plugin = {
-    name: "enforce-status-property-name",
-    rules: {
-      "status-property-name": {
-        create(context) {
-          return {
-            Identifier(node) {
-                if (node.name === "success") {
-                    context.report({
-                        node,
-                        message: "Status property name should be called 'ok' instead of 'success'",
-                        fix: (fixer) => fixer.replaceText(node, "ok"),
-                    });
-                }
-            },
-        },
+  name: "enforce-status-property-name",
+  rules: {
+    "status-property-name": {
+      create(context) {
+        return {
+          Identifier(node) {
+            if (node.name === "success") {
+              context.report({
+                node,
+                message:
+                  "Status property name should be called 'ok' instead of 'success'",
+                fix: (fixer) => fixer.replaceText(node, "ok"),
+              });
+            }
+          },
+        };
       },
     },
-    },
-  };
-  export default plugin;
-  
+  },
+};
+export default plugin;

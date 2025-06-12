6fa5e5a975f175f6ef26428e010e11c060d54b89
Marcos Candeia <marrcooos@gmail.com>
2025-06-12 19:35:51 -0300

    Revert "Improves outbound worker (#523)"
    This reverts commit 935f268240fa1f845c7ea471d7943d24d03993df.

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -3,6 +3,7 @@ import { Hono } from "hono";
 import { timing } from "hono/timing";
 import api from "./api.ts";
 import apps from "./apps.ts";
+import outbound from "./outbound.ts";
 import { AppEnv } from "./utils/context.ts";
 
 export const APPS_DOMAIN_QS = "app_host";
@@ -15,26 +16,44 @@ export const appsDomainOf = (req: Request, url?: URL) => {
     (referer && new URL(referer).searchParams.get(APPS_DOMAIN_QS));
 };
 
-const normalizeHost = (req: Request) => {
+const normalizeHost = (req: Request, env?: AppEnv["Bindings"]) => {
   const host = req.headers.get("host") ?? new URL(req.url).hostname ??
     "localhost";
+
+  const originIsScript = typeof env?.DECO_CHAT_APP_ORIGIN === "string";
+  const isUnauthorized = !req.headers.has("authorization");
+
+  const wellKnownHost = ({
+    [Hosts.API]: Hosts.API,
+    localhost: Hosts.API,
+    "localhost:3001": Hosts.API,
+    "localhost:8000": Hosts.API,
+  })[host];
+
+  const hostIsWellKnown = wellKnownHost !== undefined ||
+    host.endsWith(Hosts.APPS);
+
+  // authorize the request using outbound worker.
+  if (originIsScript && isUnauthorized && wellKnownHost === Hosts.API) {
+    return Hosts.APPS_OUTBOUND;
+  }
+
+  // send to outbound any request that is not to a well known host.
+  if (originIsScript && !hostIsWellKnown) {
+    return Hosts.APPS_OUTBOUND;
+  }
 
   const appsHost = appsDomainOf(req);
   if (appsHost) {
     return Hosts.APPS;
   }
-  return {
-    [Hosts.API]: Hosts.API,
-    localhost: Hosts.API,
-    "localhost:3001": Hosts.API,
-    "localhost:8000": Hosts.API,
-  }[host] ?? Hosts.APPS;
+  return wellKnownHost ?? Hosts.APPS;
 };
 
 export const app = new Hono<AppEnv>({
-  getPath: (req) =>
+  getPath: (req, env) =>
     "/" +
-    normalizeHost(req) +
+    normalizeHost(req, env?.env) +
     req.url.replace(/^https?:\/\/[^/]+(\/[^?]*)(?:\?.*)?$/, "$1"),
 });
 
@@ -42,4 +61,5 @@ app.use(timing({ crossOrigin: true, total: true }));
 
 app.route(`/${Hosts.API}`, api);
 app.route(`/${Hosts.APPS}`, apps);
+app.route(`/${Hosts.APPS_OUTBOUND}`, outbound);
 export default app;

ddd51d96afa22a5124e97c013fc48e7924069680
igorbrasileiro <brasileiro456@gmail.com>
2025-07-08 09:33:08 -0300

    use react-scan from vite plugin

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,5 +1,4 @@
 import "./polyfills.ts";
-import { scan } from "react-scan";
 
 import {
   ForbiddenError,
@@ -19,14 +18,6 @@ import {
 } from "react-router";
 import { EmptyState } from "./components/common/empty-state.tsx";
 import { useWorkspaceLink } from "./hooks/use-navigate-workspace.ts";
-
-if (import.meta.env.MODE === "development") {
-  const enableReactScan = !import.meta.env.VITE_ENABLE_REACT_SCAN ||
-    import.meta.env.VITE_ENABLE_REACT_SCAN === "true";
-  scan({
-    enabled: enableReactScan,
-  });
-}
 
 type LazyComp<P> = Promise<{
   default: React.ComponentType<P>;

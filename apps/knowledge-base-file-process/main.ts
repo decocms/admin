6d7ea7c12550480cb4caad944aa84ec43b6849e0
igorbrasileiro <brasileiro456@gmail.com>
2025-07-14 09:05:52 -0300

    drop workflow

--- apps/knowledge-base-file-process/main.ts
+++
@@ -1,9 +0,0 @@
-import { withRuntime } from "@deco/workers-runtime";
-import { createFileProcessingWorkflow } from "./src/workflows/file-processing.ts";
-
-const { Workflow, ...app } = withRuntime({
-  workflows: [createFileProcessingWorkflow],
-});
-
-export { Workflow };
-export default app;

93b9c97d2142911a1dd070eb98f1096103ab4de6
viktormarinho <viktormpcs@gmail.com>
2025-08-06 12:19:21 -0300

    wip

--- apps/api/main.ts
+++ apps/api/main.ts
@@ -2,8 +2,8 @@
 export * from "./src/actors.ts";
 import { contextStorage } from "@deco/sdk/fetch";
 import { Hosts } from "@deco/sdk/hosts";
-import { instrument } from "@deco/sdk/observability";
-import { getRuntimeKey } from "hono/adapter";
+// import { instrument } from "@deco/sdk/observability";
+// import { getRuntimeKey } from "hono/adapter";
 import { default as app } from "./src/app.ts";
 import { email } from "./src/email.ts";
 import { KbFileProcessorWorkflow } from "./src/workflows/kb-file-processor-workflow.ts";
@@ -12,16 +12,8 @@ export { WorkspaceDatabase } from "./src/durable-objects/workspace-database.ts";
 import * as Sentry from "@sentry/cloudflare";
 
 // Choose instrumented app depending on runtime
-const instrumentedApp = getRuntimeKey() === "deno" ? app : instrument(app);
+// const instrumentedApp = getRuntimeKey() === "deno" ? app : instrument(app);
 
-// Domains we consider "self"
-const SELF_DOMAINS: string[] = [
-  Hosts.API,
-  // @ts-expect-error env is not typed
-  ...(env.VITE_USE_LOCAL_BACKEND ? [] : [Hosts.APPS]),
-  // @ts-expect-error env is not typed
-  `localhost:${env.PORT || 8000}`,
-];
 
 // Patch fetch globally
 const originalFetch = globalThis.fetch;
@@ -35,39 +27,51 @@ const originalFetch = globalThis.fetch;
  * This ensures internal requests work as expected in both Deno and Cloudflare environments.
  */
 // @ts-ignore: mixed cloudflare and deno types
-globalThis.fetch = async function patchedFetch(
-  resource: RequestInfo | URL,
-  init?: RequestInit,
-): Promise<Response> {
-  let req: Request;
-  if (typeof resource === "string") {
-    req = new Request(resource, init);
-  } else if (resource instanceof Request) {
-    req = resource;
-  } else if (resource instanceof URL) {
-    req = new Request(resource.toString(), init);
-  } else {
-    throw new Error("Unsupported resource type for fetch");
-  }
+// globalThis.fetch = async function patchedFetch(
+//   resource: RequestInfo | URL,
+//   init?: RequestInit,
+// ): Promise<Response> {
+//   let req: Request;
+//   if (typeof resource === "string") {
+//     req = new Request(resource, init);
+//   } else if (resource instanceof Request) {
+//     req = resource;
+//   } else if (resource instanceof URL) {
+//     req = new Request(resource.toString(), init);
+//   } else {
+//     throw new Error("Unsupported resource type for fetch");
+//   }
 
-  const url = new URL(req.url);
+//   const url = new URL(req.url);
 
-  const context = contextStorage.getStore();
+//   const context = contextStorage.getStore();
 
-  if (SELF_DOMAINS.some((domain) => url.host.endsWith(domain))) {
-    if (!context) {
-      throw new Error("Missing context for internal self-invocation");
-    }
-    // Delegate to internal handler
-    return await instrumentedApp.fetch!(
-      req as Request<unknown, IncomingRequestCfProperties<unknown>>,
-      context.env,
-      context.ctx,
-    );
-  }
+//   if (SELF_DOMAINS.some((domain) => url.host.endsWith(domain))) {
+//     if (!context) {
+//       throw new Error("Missing context for internal self-invocation");
+//     }
+//     // Delegate to internal handler
+//     return await app.fetch(
+//       req as Request<unknown, IncomingRequestCfProperties<unknown>>,
+//       context.env,
+//       context.ctx,
+//     );
+//   }
 
-  return await originalFetch(req);
-};
+//   return await originalFetch(req);
+// };
+
+// Domains we consider "self"
+const SELF_DOMAINS: string[] = [
+  Hosts.API,
+  // @ts-expect-error env is not typed
+  ...(env.VITE_USE_LOCAL_BACKEND ? [] : [Hosts.APPS]),
+  // @ts-expect-error env is not typed
+  `localhost:${env.PORT || 8000}`,
+];
+
+//@ts-ignore
+console.log("Original fetch before export default", originalFetch.__sentry_original__);
 
 // Default export that wraps app with per-request context initializer
 export default Sentry.withSentry(
@@ -77,6 +81,11 @@ export default Sentry.withSentry(
     // Learn more at
     // https://docs.sentry.io/platforms/javascript/configuration/options/#traces-sample-rate
     tracesSampleRate: 1.0,
+
+    integrations: [
+      Sentry.consoleLoggingIntegration({ levels: ["error", "warn", "log"] }),
+      Sentry.fetchIntegration(),
+    ],
 
     // Send structured logs to Sentry
     enableLogs: true,
@@ -92,8 +101,9 @@ export default Sentry.withSentry(
       env: any,
       ctx: ExecutionContext,
     ): Promise<Response> {
+      console.log("fetch", request.url);
       return contextStorage.run({ env, ctx }, async () => {
-        return await instrumentedApp.fetch!(
+        return await app.fetch(
           request as Request<unknown, IncomingRequestCfProperties<unknown>>,
           env,
           ctx,
@@ -102,6 +112,9 @@ export default Sentry.withSentry(
     },
   },
 );
+
+//@ts-ignore
+console.log("Original fetch after export default", originalFetch.__sentry_original__);
 
 // Export the workflow
 export { KbFileProcessorWorkflow };
--- apps/api/src/actors.ts
+++ apps/api/src/actors.ts
@@ -1,6 +1,16 @@
 export { AIAgent, Trigger };
 import { AIAgent as _AIAgent, Trigger as _Trigger } from "@deco/ai/actors";
-import { instrumentDO } from "@deco/sdk/observability";
+// import { instrumentDO } from "@deco/sdk/observability";
+import * as Sentry from "@sentry/cloudflare";
 
-const AIAgent = instrumentDO(_AIAgent);
-const Trigger = instrumentDO(_Trigger);
+const AIAgent = Sentry.instrumentDurableObjectWithSentry(
+    (env: any) => ({
+        dsn: "https://15a2257ed06466face415d9d6faa4740@o4509797555503104.ingest.us.sentry.io/4509797557141504",
+        tracesSampleRate: 1.0,
+        integrations: [
+            Sentry.consoleLoggingIntegration({ levels: ["error", "warn", "log"] }),
+            Sentry.fetchIntegration(),
+          ],
+      }),
+    _AIAgent as any);
+const Trigger = (_Trigger);

3a7fb0d7d8816ead57df4fd6a2541d6c2eb50ea2
gimenes <tlgimenes@gmail.com>
2025-08-06 00:27:17 -0300

    allow tmp migration

--- apps/api/src/api.ts
+++ apps/api/src/api.ts
@@ -43,8 +43,16 @@ export const honoCtxToAppCtx = (c: Context<AppEnv>): AppContext => {
   const policyClient = PolicyClient.getInstance(c.var.db);
   const authorizationClient = new AuthorizationClient(policyClient);
 
+  /**
+   * TODO: Remove after migration is finished
+   * Temporary solution to bypass auth to allow gimenes to migrate dbs
+   */
+  const isLocal = typeof c.var?.user?.email === "string" &&
+    c.var.user.email.includes("@deco.cx");
+
   return {
     ...c.var,
+    isLocal,
     params: { ...c.req.query(), ...c.req.param() },
     envVars: envs,
     cookie: c.req.header("Cookie"),
@@ -95,16 +103,14 @@ const createMCPHandlerFor = (
         {
           annotations: tool.annotations,
           description: tool.description,
-          inputSchema:
-            "shape" in tool.inputSchema
-              ? (tool.inputSchema.shape as z.ZodRawShape)
-              : z.object({}).shape,
-          outputSchema:
-            tool.outputSchema &&
-            typeof tool.outputSchema === "object" &&
-            "shape" in tool.outputSchema
-              ? (tool.outputSchema.shape as z.ZodRawShape)
-              : z.object({}).shape,
+          inputSchema: "shape" in tool.inputSchema
+            ? (tool.inputSchema.shape as z.ZodRawShape)
+            : z.object({}).shape,
+          outputSchema: tool.outputSchema &&
+              typeof tool.outputSchema === "object" &&
+              "shape" in tool.outputSchema
+            ? (tool.outputSchema.shape as z.ZodRawShape)
+            : z.object({}).shape,
         },
         // @ts-expect-error: zod shape is not typed
         withMCPErrorHandling(tool.handler),
@@ -288,8 +294,8 @@ app.get("/files/:root/:slug/:path{.+}", async (c) => {
   }
 
   return c.body(response.body, 200, {
-    "Content-Type":
-      response.headers.get("content-type") || "application/octet-stream",
+    "Content-Type": response.headers.get("content-type") ||
+      "application/octet-stream",
   });
 });
 

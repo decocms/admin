61e5caa789bb2ce58b94e97cb3a1817c51c5afa6
viktormarinho <viktormpcs@gmail.com>
2025-08-18 13:31:09 -0300

    fmt

--- packages/runtime/src/drizzle.ts
+++ packages/runtime/src/drizzle.ts
@@ -52,7 +52,10 @@ export const enviromentForFixedDatabaseBinding = (binding: {
 
 export function drizzle<
   TSchema extends Record<string, unknown> = Record<string, never>,
->({ DECO_CHAT_WORKSPACE_DB }: DrizzleEnvironment, config?: DrizzleConfig<TSchema>) {
+>(
+  { DECO_CHAT_WORKSPACE_DB }: DrizzleEnvironment,
+  config?: DrizzleConfig<TSchema>,
+) {
   return drizzleProxy((sql, params, method) => {
     // https://orm.drizzle.team/docs/connect-drizzle-proxy says
     // Drizzle always waits for {rows: string[][]} or {rows: string[]} for the return value.

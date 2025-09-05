f7cf02c6167aad17b82e9325baab0bae31b7b948
viktormarinho <viktormpcs@gmail.com>
2025-09-05 16:44:02 -0300

    wip banner

--- apps/web/src/components/common/event/deco-day.tsx
+++ apps/web/src/components/common/event/deco-day.tsx
@@ -1,5 +1,7 @@
 import { cn } from "@deco/ui/lib/utils.ts";
 import { EventBanner, useCountdown } from "../event-banner";
+import { Button } from "@deco/ui/components/button.tsx";
+import { useState } from "react";
 
 const DECO_DAY_START_DATE = new Date("2025-09-08T14:00:00");
 const DECO_DAY_END_DATE = new Date("2025-09-08T18:00:00");
@@ -43,10 +45,23 @@ const Upcoming = () => {
       target="_blank"
       rel="noopener noreferrer"
       className={cn(
-        "bg-[url('/img/deco-day-upcoming.png')] bg-cover h-[120px] rounded-lg ring ring-border overflow-hidden hover:ring-4 transition-all duration-400",
+        "bg-[url('/img/deco-day-upcoming-bg.svg')] bg-cover h-[120px] rounded-lg ring ring-border overflow-hidden hover:ring-4 transition-all duration-400 flex items-center",
       )}
     >
-      <div className="flex justify-end pt-10 pr-56 gap-3">
+      <div className="flex flex-col items-start self-end shrink-0">
+        <span className="px-6 bg-black text-white">@techweeksaopaulo</span>
+        <span className="px-6 py-4 bg-black text-white text-[28px]">
+          Learn from industry leaders
+        </span>
+      </div>
+
+      <img
+        src="/img/deco-day-logo-outline.svg"
+        alt="deco.day upcoming"
+        className="h-[50px] object-cover ml-auto mr-4"
+      />
+
+      <div className="flex justify-end gap-3 ml-auto">
         <CountdownBox value={days} label="days" />
         <Separator />
         <CountdownBox value={hours} label="hours" />
@@ -55,6 +70,10 @@ const Upcoming = () => {
         <Separator />
         <CountdownBox value={seconds} label="seconds" />
       </div>
+
+      <Button variant="special" className="ml-auto mr-4">
+        <span>Watch on YouTube</span>
+      </Button>
     </a>
   );
 };
@@ -84,13 +103,43 @@ const Past = () => {
 };
 
 export function DecoDayBanner() {
+  const [state, setState] = useState<"upcoming" | "active" | "past">(
+    "upcoming",
+  );
+
+  const cycle = () => {
+    setState((prev) => {
+      if (prev === "upcoming") return "active";
+      if (prev === "active") return "past";
+      return "upcoming";
+    });
+  };
+
   return (
-    <EventBanner
-      startDate={DECO_DAY_START_DATE}
-      endDate={DECO_DAY_END_DATE}
-      upcoming={<Upcoming />}
-      active={<Active />}
-      past={<Past />}
-    />
+    <>
+      <EventBanner
+        startDate={DECO_DAY_START_DATE}
+        endDate={DECO_DAY_END_DATE}
+        upcoming={
+          state === "upcoming" ? (
+            <Upcoming />
+          ) : state === "active" ? (
+            <Active />
+          ) : (
+            <Past />
+          )
+        }
+        active={<Active />}
+        past={<Past />}
+      />
+      <div className="fixed top-4 right-4">
+        <button
+          className="cursor-pointer p-1 border border-border rounded-md bg-background"
+          onClick={cycle}
+        >
+          cycle
+        </button>
+      </div>
+    </>
   );
 }

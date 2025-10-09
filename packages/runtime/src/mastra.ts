5999e4c0d00095cc67a10278b03f6d693878d833
guitavano <tavano62@gmail.com>
2025-10-09 12:01:21 -0300

    fix oauth definition tool

--- packages/runtime/src/mastra.ts
+++ packages/runtime/src/mastra.ts
@@ -757,6 +757,12 @@ export const createMCPServer = <
     // Finally append resource tools after other tools are collected
     tools.push(...resourceTools);
 
+    const hasOAuthTools = tools.some((t) => t.id === "DECO_CHAT_OAUTH_START");
+
+    if (!hasOAuthTools) {
+      tools.push(...decoChatOAuthToolsFor<TSchema>(options.oauth));
+    }
+
     for (const tool of tools) {
       server.registerTool(
         tool.id,
@@ -790,11 +796,6 @@ export const createMCPServer = <
           };
         },
       );
-    }
-
-    const hasOAuthTools = tools.some((t) => t.id === "DECO_CHAT_OAUTH_START");
-    if (!hasOAuthTools) {
-      tools.push(...decoChatOAuthToolsFor<TSchema>(options.oauth));
     }
 
     return { server, tools };

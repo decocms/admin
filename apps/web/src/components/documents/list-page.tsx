294f09e9a406abbaf4c9048a81bfb7d438aa8f14
guitavano <tavano62@gmail.com>
2025-10-14 00:35:43 -0300

    avoid show legacy option to users that dont need

--- apps/web/src/components/documents/list-page.tsx
+++ apps/web/src/components/documents/list-page.tsx
@@ -1,8 +1,16 @@
+import { usePrompts } from "@deco/sdk";
 import { DocumentsResourceList } from "./documents-resource-list.tsx";
 import { DocumentsTabs } from "./tabs-nav.tsx";
 
 export default function DocumentsListPage() {
+  // it is being used to not pollute the screen for new users
+  // as soon as no one uses the old prompts anymore, we can remove it
+  // to not fetch it unnecessarily
+  const { data: legacyPrompts } = usePrompts();
+  const hideLegacy = legacyPrompts?.length <= 2;
   return (
-    <DocumentsResourceList headerSlot={<DocumentsTabs active="documents" />} />
+    <DocumentsResourceList
+      headerSlot={hideLegacy ? undefined : <DocumentsTabs active="documents" />}
+    />
   );
 }

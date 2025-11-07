646dc5213b90cdd3a4db7d782e9c2aa4694770f4
viktormarinho <viktormpcs@gmail.com>
2025-11-07 16:00:19 -0300

    wip

--- apps/mesh/vite.config.ts
+++ apps/mesh/vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  server: {
+    port: 4000,
+  },
+  plugins: [
+    react(),
+    tailwindcss(),
+    tsconfigPaths({
+      root: ".",
+    }),
+  ],
 });

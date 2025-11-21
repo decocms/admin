d143003b6b6f5470001166ed3c36d94cb67c8046
gimenes <tlgimenes@gmail.com>
2025-11-21 13:37:36 -0300

    ts check

--- packages/ai/src/mcp.ts
+++ packages/ai/src/mcp.ts
@@ -88,8 +88,7 @@ const getMCPServerTools = async (
     const mtools: Record<string, Tool> = Object.fromEntries(
       tools.map((tool: (typeof tools)[number]) => {
         const slug = slugify(tool.name);
-
-        // @ts-expect-error - tool.inputSchema is not a JSONSchema7
+        
         const inputSchema = patchedJsonSchema(tool.inputSchema);
         const outputSchema = patchedJsonSchema(
           tool.outputSchema ?? {

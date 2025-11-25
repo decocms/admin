d97082bd26547c6c813bca64dd1a7d354b26010f
Marcos Candeia <marrcooos@gmail.com>
2025-11-25 11:53:33 -0300

    Fixes migration
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>

--- apps/mesh/migrations/index.ts
+++ apps/mesh/migrations/index.ts
@@ -1,10 +1,10 @@
 import { type Migration } from "kysely";
-import * as migration001 from "./001-initial-schema.ts";
-import * as migration002 from "./002-organization-settings.ts";
+import * as migration001initialschema from "./001-initial-schema.ts";
+import * as migration002organizationsettings from "./002-organization-settings.ts";
 
 const migrations = {
-  "001-initial-schema": migration001,
-  "002-organization-settings": migration002,
+  "001-initial-schema": migration001initialschema,
+  "002-organization-settings": migration002organizationsettings,
 } satisfies Record<string, Migration>;
 
 export default migrations;

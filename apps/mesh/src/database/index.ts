06d2a96891f68b73ef22f2c827e3321bc73f836e
Marcos Candeia <marrcooos@gmail.com>
2025-11-26 18:19:34 -0300

    Remove ssl for pg pool
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>

--- apps/mesh/src/database/index.ts
+++ apps/mesh/src/database/index.ts
@@ -74,10 +74,7 @@ class PostgresDatabase implements SupportedDatabase {
       pool: new Pool({
         connectionString: config.connectionString,
         max: config.options?.maxConnections || 10,
-        ssl: {
-          // unsafe for now.
-          rejectUnauthorized: false, // <-- bypass verification
-        },
+        ssl: false,
       }),
     });
   }

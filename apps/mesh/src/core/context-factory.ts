c8f82bfec727e014a805eebbb151c2a341657217
Marcos Candeia <marrcooos@gmail.com>
2025-12-12 18:42:46 -0300

    Adds organization context
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>

--- apps/mesh/src/core/context-factory.ts
+++ apps/mesh/src/core/context-factory.ts
@@ -71,8 +71,8 @@ interface OAuthSession {
 
 interface OrganizationContext {
   id: string;
-  slug: string;
-  name: string;
+  slug?: string;
+  name?: string;
 }
 
 interface AuthenticatedUser {
@@ -447,6 +447,11 @@ async function authenticateRequest(
         return {
           user: { id: meshJwtPayload.sub },
           permissions: meshJwtPayload.permissions,
+          organization: meshJwtPayload.metadata?.organizationId
+            ? {
+                id: meshJwtPayload.metadata?.organizationId,
+              }
+            : undefined,
         };
       }
     } catch {
